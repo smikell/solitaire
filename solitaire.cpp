fb0df85afdb6dffb8368d5e1ac1734411799bcc3
smikell <smikell@umich.edu>
2019-12-19 22:53:25 -0600

    added driver cpp file

---
+++ solitaire.cpp
@@ -0,0 +1,14 @@
+// Solitaire for the Bored Programmer
+//
+// by Sam Mikell
+//
+// 19 December 2019
+
+#include <iostream>
+
+int main() {
+    
+    std::cout << "Hello World\n";
+    
+    return 0;
+}
